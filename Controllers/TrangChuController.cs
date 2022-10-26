c35e54faab8779c0b176dde524e8d73308ff32dc
Admin <Admin@LAPTOP-57VFA84R>
2022-10-26 19:14:07 +0700

    Add project files.

---
+++ Controllers/TrangChuController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebBanSach.Controllers
+{
+    public class TrangChuController : Controller
+    {
+        public ActionResult Home()
+        {
+            return View();
+        }
+
+        public ActionResult TaiKhoanBtn()
+        {
+            return View();
+        }
+
+        public ActionResult TieuThuyetBtn()
+        {
+            return View();
+        }
+
+        public ActionResult TruyenTranhBtn()
+        {
+            return View();
+        }
+
+        public ActionResult SachLapTrinhBtn()
+        {
+            return View();
+        }
+    }
+}
