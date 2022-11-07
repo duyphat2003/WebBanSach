3c5548b173b1e2f233519b546d5264d2c79fb957
duyphat2003 <89205718+duyphat2003@users.noreply.github.com>
2022-11-07 15:07:06 +0700

    Thay đổi  giao diện của Lượng vào web với kèm bootstrap sản phẩm tạm (Nhìn không được thì comment trong group)

--- Controllers/TrangChuController.cs
+++ Controllers/TrangChuController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using WebBanSach.Models;
@@ -16,6 +17,20 @@ namespace WebBanSach.Controllers
         {
             var sACHes = db.SACHes.Include(s => s.CHUDE).Include(s => s.NHAXUATBAN);
             return View(sACHes.ToList());
+        }
+
+        public ActionResult ChiTiet(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SACH product = db.SACHes.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
         }
     }
 }
