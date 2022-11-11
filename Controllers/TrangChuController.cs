37ab17feb74c06c900b9c59f82517285178f458f
duyphat2003 <89205718+duyphat2003@users.noreply.github.com>
2022-11-11 17:09:42 +0700

    Mới chỉnh sửa lại phần div-center-info với trang chi tiết.

--- Controllers/TrangChuController.cs
+++ Controllers/TrangChuController.cs
@@ -13,12 +13,23 @@ namespace WebBanSach.Controllers
     public class TrangChuController : Controller
     {
         private QLBANSACHEntities db = new QLBANSACHEntities();
+        [HttpGet]
         public ActionResult Home()
         {
             var sACHes = db.SACHes.Include(s => s.CHUDE).Include(s => s.NHAXUATBAN);
             return View(sACHes.ToList());
         }
-
+        [HttpPost]
+        public ActionResult Home(string searchQuery)
+        {
+            var sachs = from sach in db.SACHes select sach;
+            if(!string.IsNullOrEmpty(searchQuery))
+            {
+                sachs = sachs.Where(s => s.Tensach.Contains(searchQuery));
+            }
+            return View(sachs);
+        }
+                                         
         public ActionResult ChiTiet(int? id)
         {
             if (id == null)
---
+++ Models/CartItem.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebBanSach.Models
+{
+    public class CartItem
+    {
+        QLBANSACHEntities db = new QLBANSACHEntities();
+        public int MaSach { get; set; }
+        public string TenSach { get; set; }
+        public string ImageFile { get; set; }
+        public decimal Price { get; set; }
+        public int Number { get; set; }
+        public decimal Total()
+        {
+            return Number * Price;
+        }
+        public CartItem(int MaSach, int Amount)
+        {
+            this.MaSach = MaSach;
+            var sachDB = db.SACHes.Single(s => s.Masach == this.MaSach);
+            this.TenSach = sachDB.Tensach;
+            this.ImageFile = sachDB.Hinhminhhoa;
+            this.Price = (decimal)sachDB.Dongia;
+            this.Number = Amount;
+        }
+    }
+}
